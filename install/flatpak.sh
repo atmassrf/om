71effc32e219628f989eea46622997deed2ca7e1
atmassrf <161273947+atmassrf@users.noreply.github.com>
2024-06-19 10:23:56 +0000

    Update flatpak.sh

--- install/flatpak.sh
+++ install/flatpak.sh
@@ -1,3 +1,11 @@
 sudo apt install -y flatpak
 sudo apt install -y gnome-software-plugin-flatpak
 sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+
+# INstall Flatseal to sandbox and limit Flatpack containers
+flatpak install flathub com.github.tchx84.Flatseal
+# flatpak run com.github.tchx84.Flatseal
+# TODO configure Obsidian Flatpak settings to limit access to networking and filesystem 
+flatpak install flathub md.obsidian.Obsidian
+# flatpak run md.obsidian.Obsidian
+# TODO add this to the Dock or make it run once to configure
